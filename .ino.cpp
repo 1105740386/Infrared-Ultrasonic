c9bcd6c86fcd118c6fdec971910f98478120d30b
ZhiChao Wang <1105740386qq.com>
2016-07-07 21:55:14 +0800

    实现红外控制继电器，超声波控制继电器

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-07-07 00:08:04
+//This file has been generated on 2016-07-07 21:51:08
 
 #include "Arduino.h"
 #include <Arduino.h>
@@ -11,7 +11,10 @@
 #include "header/IRremote/IRremote.h"
 void setup() ;
 void loop() ;
-void controlMotor(String radarValue) ;
+void controlMotorWithInfraredSignal(String currentRadarValue) ;
+void controlMotorWithUltrasonicSignal(unsigned long distance) ;
+String getInfraredData() ;
+unsigned long getUltrasonicData() ;
 
 
 #include "AnalogReadSerial.ino"
--- AnalogReadSerial.ino
+++ AnalogReadSerial.ino
@@ -10,17 +10,20 @@
 #define PIN_YELLOW 4
 #define PIN_RADAR 10
 #define PIN_RELAY 11
+#define ECHO  8
+#define TE1G  9
+#define MIN_DISTANCE 20
 
-// leds
 int leds[3] = { 2, 3, 4 };
-
-// radar
 IRrecv irrecv(PIN_RADAR);
 decode_results results;
-
-//
 const String RADAR_OK = "111111110000001011111101";
+String oldRadarValue;
+int relayState = 0;
 
+/**
+ * 初始化函数，只在电源接通时执行一次
+ */
 void setup() {
 	Serial.begin(9600);
 
@@ -28,21 +31,22 @@ void setup() {
 	pinMode(PIN_GREEN, OUTPUT);
 	pinMode(PIN_YELLOW, OUTPUT);
 	pinMode(PIN_RELAY, OUTPUT);
-
+	pinMode(ECHO, INPUT);
+	pinMode(TE1G, OUTPUT);
 	irrecv.enableIRIn();
 }
 
+/**
+ * Arduino循环函数，每秒钟被执行1W次
+ */
 void loop() {
 
-	if (irrecv.decode(&results)) {
-		//Serial.println(results.value, BIN);
-		String tempRadarValue = String(results.value, BIN);
-		Serial.println(tempRadarValue);
+	String currentRadarValue = getInfraredData();
 
-		controlMotor(tempRadarValue);
+	unsigned long distance = getUltrasonicData();
 
-		irrecv.resume();
-	}
+	controlMotorWithInfraredSignal(currentRadarValue);
+	controlMotorWithUltrasonicSignal(distance);
 
 	for (int i = 0; i <= 2; i++) {
 		digitalWrite(leds[i], HIGH);
@@ -52,10 +56,70 @@ void loop() {
 	}
 }
 
-void controlMotor(String radarValue) {
-	if (radarValue.equals(RADAR_OK)) {
+/**
+ * 用红外信号控制继电器，按下OK开启接通继电器，启动马达
+ */
+void controlMotorWithInfraredSignal(String currentRadarValue) {
+	if (currentRadarValue.equals(RADAR_OK) && !oldRadarValue.equals(RADAR_OK)) {
+		relayState = 1 - relayState;
+		delay(100);
+	}
+
+	oldRadarValue = currentRadarValue;
+	Serial.print("oldRadarValue = ");
+	Serial.println(oldRadarValue);
+
+	if (relayState == 1) {
 		digitalWrite(PIN_RELAY, LOW);
 	} else {
 		digitalWrite(PIN_RELAY, HIGH);
 	}
+}
+
+/**
+ * 用超声波信号控制继电器，距离小于20CM，断开继电器，断开马达电源
+ */
+void controlMotorWithUltrasonicSignal(unsigned long distance) {
+
+	int relayVoltage = digitalRead(PIN_RELAY);
+	Serial.print("relayVoltage = ");
+	Serial.println(relayVoltage);
+
+	if (relayVoltage == LOW && distance <= MIN_DISTANCE) {
+		digitalWrite(PIN_RELAY, HIGH);
+	}
+}
+
+/**
+ * 读取红外信号数据
+ */
+String getInfraredData() {
+	String currentRadarValue;
+	if (irrecv.decode(&results)) {
+		currentRadarValue = String(results.value, BIN);
+		irrecv.resume();
+		Serial.println(currentRadarValue);
+		delay(100);
+	}
+	return currentRadarValue;
+}
+
+/**
+ * 读取超声波数据
+ */
+unsigned long getUltrasonicData() {
+	// 使发出发出超声波信号接口低电平2μs
+	digitalWrite(TE1G, LOW);
+	delayMicroseconds(2);
+	// 使发出发出超声波信号接口高电平10μs，这里是至少10μs
+	digitalWrite(TE1G, HIGH);
+	delayMicroseconds(10);
+	// 保持发出超声波信号接口低电平
+	digitalWrite(TE1G, LOW);
+	delayMicroseconds(2);
+	// 读出脉冲时间 // 将脉冲时间转化为距离（单位：厘米）
+	unsigned long distance = pulseIn(ECHO, HIGH) / 58;
+	//输出距离值
+	Serial.println(distance);
+	return distance;
 }
